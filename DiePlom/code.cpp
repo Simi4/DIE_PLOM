9e0376f50a72422b5fcaf9e4bf3397c287cd2e27
Smirnov Maxim <Maxim.like.Python@ya.ru>
2019-05-03 17:18:24 +0300

    Fixes

--- DiePlom/code.cpp
+++ DiePlom/code.cpp
@@ -1,24 +1,16 @@
+#ifdef _WIN32
 #include <windows.h>
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <limits>
-#include <random>
-#include <cmath>
+#endif
 #include <algorithm>
 #include <array>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
 #include <numeric>
-#include <cassert>
-
-
-using namespace std;
-
-// для проверки алгоритма
-// #define CHECK_ALG
-#define SEED (unsigned)time(nullptr)
-
-// Задаем точность до сотых
-constexpr auto PRECISION = 3;
+#include <vector>
+#include <random>
 
 
 // кол-во многоканальных РЛС ( i = 1, ..., K )
@@ -27,6 +19,19 @@ const auto K = 3;
 // кол-во ГЦ ( j = 1, ..., N )
 const auto NUM_GC = 3;
 auto N = NUM_GC;
+
+
+using namespace std;
+
+// Зерно генератора случайных чисел
+#define SEED (unsigned)time(nullptr)
+
+// Задаем точность вывода до сотых
+const auto PRECISION = 3;
+
+
+// Генератор случайных чисел
+default_random_engine gen(SEED);
 
 
 // Тип данных - массив N-элементов
@@ -48,7 +53,7 @@ ostream& operator<< (ostream& os, const array_j& val)
 {
 	for (auto it = begin(val); it != begin(val) + N; it++)
 	{
-		os << setw(10) << *it << " ";
+		os << setw(8 + PRECISION) << *it;
 	}
 	return os;
 }
@@ -58,7 +63,7 @@ ostream& operator<< (ostream& os, const array_i& val)
 {
 	for (auto it = begin(val); it != begin(val) + K; it++)
 	{
-		os << setw(10) << *it << " ";
+		os << setw(8 + PRECISION) << *it;
 	}
 	return os;
 }
@@ -66,7 +71,7 @@ ostream& operator<< (ostream& os, const array_i& val)
 // Функция вывода матрицы
 ostream& operator<< (ostream& os, const matrix_ij& val)
 {
-	for (const auto& row : val)
+	for (const array_j& row : val)
 	{
 		os << row << endl;
 	}
@@ -77,69 +82,50 @@ ostream& operator<< (ostream& os, const matrix_ij& val)
 // важность j-ой ГЦ ( 0 <= Vj <= 100 )
 array_j V;
 
+// Функция заполнения массива Vj целыми случайными числам 0 .. 100
+void fill_V()
+{
+	uniform_int_distribution<unsigned> dist(0, 100);
+	generate(begin(V), begin(V) + N, bind(dist, gen));
+}
+
 
 // потребный радиолокационный потенциал, необходимый для обслуживания всех целей в ГЦ ( 0 <= Qj <= 1000 )
 array_j Q;
 
+// Функция заполнения массива Qj целыми случайными числам 0 .. 1000
 void fill_Q()
 {
-#ifndef CHECK_ALG
-	// Генератор случайных чисел
-	static default_random_engine gen(SEED);
-	static uniform_int_distribution<unsigned> dist(0, 1000);
-
-	for (int j = 0; j < N; j++)
-	{
-		Q[j] = dist(gen);
-	}
-#else
-	Q = { 300, 200, 100 };
-#endif
+	uniform_int_distribution<unsigned> dist(0, 1000);
+	generate(begin(Q), begin(Q) + N, bind(dist, gen));
 }
 
 
 // располагаемый радиолокационный потенциал РЛС, необходимый для сопровождения и поиск всех целей в ГЦ ( 0 <= Wi <= 5000 )
 array_i W;
 
+// Функция заполнения массива Wi целыми случайными числам 0 .. 5000
 void fill_W()
 {
-#ifndef CHECK_ALG
-	// Генератор случайных чисел
-	static default_random_engine gen(SEED);
-	static uniform_int_distribution<unsigned> dist(0, 5000);
-
-	for (int i = 0; i < K; i++)
-	{
-		W[i] = dist(gen);
-	}
-#else
-	W = { 100, 200, 300 };
-#endif
+	uniform_int_distribution<unsigned> dist(0, 5000);
+	generate(begin(W), end(W), bind(dist, gen));
 }
 
 
 // потенциал i-ой РЛС, выделяемый на j-ую ГЦ ( 0 <= wij <= 1000 )
 matrix_ij w;
 
+// Функция заполнения матрицы wij целыми случайными числам 0 .. 1000
 void fill_w()
 {
-#ifndef CHECK_ALG
-	// Генератор случайных чисел
-	static default_random_engine gen(SEED);
-	static uniform_int_distribution<unsigned> dist(0, 1000);
-
-	for (int i = 0; i < K; i++)
+	uniform_int_distribution<unsigned> dist(0, 1000);
+	for (array_j& row : w)
 	{
-		for (int j = 0; j < N; j++)
+		for (auto& val : row)
 		{
-			w[i][j] = dist(gen);
+			val = dist(gen);
 		}
 	}
-#else
-	w[0] = { 100, 200, 300 };
-	w[1] = { 200, 300, 400 };
-	w[2] = { 300, 400, 500 };
-#endif
 }
 
 
@@ -150,47 +136,23 @@ array_j q;
 // обеспечиваемая при обслуживании j-ой ГЦ важность
 array_j v;
 
-void fill_V()
-{
-#ifndef CHECK_ALG
-	// Генератор случайных чисел
-	static default_random_engine gen(SEED);
-	static uniform_int_distribution<unsigned> dist(0, 100);
-
-	for (int j = 0; j < N; j++)
-	{
-		V[j] = dist(gen);
-	}
-#else
-	V = { 20, 30, 40 };
-#endif
-}
-
 // важность, обеспечиваемая при обслуживании всех ГЦ
 double SV;
 
 // вероятность обеспечения обслуживания i-ой РЛС j-ой ГЦ ( 0 <= pij <= 1 )
 matrix_ij p;
 
+// Функция заполнения матрицы pij случайными числам 0.0 .. 1.0
 void fill_p()
 {
-#ifndef CHECK_ALG
-	// Генератор случайных чисел
-	default_random_engine gen(SEED);
 	uniform_real_distribution<double> dist(0.0, 1.0);
-
-	for (int i = 0; i < K; i++)
+	for (array_j& row : p)
 	{
-		for (int j = 0; j < N; j++)
+		for (auto& val : row)
 		{
-			p[i][j] = dist(gen);
+			val = dist(gen);
 		}
 	}
-#else
-	p[0] = { 0.5, 0.2, 0.3 };
-	p[1] = { 0.1, 0.8, 0.4 };
-	p[2] = { 0.1, 0.3, 0.7 };
-#endif
 }
 
 // важность, не обеспечиваемая при обслуживании всех ГЦ
@@ -204,10 +166,10 @@ matrix_ij C;
 // Опорный план
 matrix_ij plan;
 
-// Функция заполнения опорного плана "NaN"
+// Функция заполнения матрицы опорного плана значением "NaN"
 void fill_plan()
 {
-	for (auto& row : plan)
+	for (array_j& row : plan)
 	{
 		fill(begin(row), end(row), numeric_limits<double>::quiet_NaN());
 	}
@@ -222,11 +184,11 @@ void print_plan()
 		{
 			if (isnan(plan[i][j]))
 			{
-				cout << setw(13) << C[i][j];
+				cout << setw(12 + PRECISION) << C[i][j];
 			}
 			else
 			{
-				cout << setw(6) << C[i][j] << "[" << setw(4) << setprecision(0) << plan[i][j] << setprecision(PRECISION) << "] ";
+				cout << setw(3 + PRECISION) << C[i][j] << "[" << setw(4) << setprecision(0) << plan[i][j] << setprecision(PRECISION) << setw(4) << "]";
 			}
 		}
 		cout << endl;
@@ -237,19 +199,19 @@ void print_plan()
 // Функция вывода матрицы тарифов + запасы и потребности
 void print_full()
 {
-	cout << setprecision(PRECISION);
-	cout << setw(6 + N * 11) << "C[K][N]" << "W[K]" << endl;
+	streamsize col_width = PRECISION + 8;
+	cout << setw((streamsize)6 + (streamsize)N * col_width) << "C[K][N]" << "W[K]" << endl;
 
 	for (int i = 0; i < K; i++)
 	{
 		cout << setw(6) << "";
 		for (int j = 0; j < N; j++)
 		{
-			cout << setw(10) << C[i][j] << " ";
+			cout << setw(col_width) << C[i][j];
 		}
 		cout << setprecision(0) << W[i] << setprecision(PRECISION) << endl;
 	}
-	cout << "Q[N]  " << setprecision(0) <<  Q << setprecision(PRECISION) << endl << endl;
+	cout << setw(6) << "Q[N]" << setprecision(0) << Q << setprecision(PRECISION) << endl << endl;
 }
 
 
@@ -257,9 +219,11 @@ int main()
 {
 	srand(SEED);
 
+#ifdef _WIN32
 	// Кодировка консоли
 	SetConsoleCP(CP_UTF8);
 	SetConsoleOutputCP(CP_UTF8);
+#endif
 
 	cout << fixed << left << setprecision(PRECISION);
 
@@ -329,8 +293,7 @@ int main()
 	print_full();
 
 
-	////радиолокационный потенциал, выделенный всеми РЛС на j-ую ГЦ
-	//cout << ". q[N]: ";
+	// радиолокационный потенциал, выделенный всеми РЛС на j-ую ГЦ
 	//for (int i = 0; i < K; i++)
 	//{
 	//	for (int j = 0; j < N; j++)
@@ -338,11 +301,7 @@ int main()
 	//		q[j] += w[i][j];
 	//	}
 	//}
-	//for (int j = 0; j < N; j++)
-	//{
-	//	cout << q[j] << "\t";
-	//}
-	//cout << endl;
+	//cout << "q[N]: " << q << endl;
 
 	// потенциал i-ой РЛС, выделяемый на j-ую ГЦ (0<=wij<=1000)
 	fill_w();
@@ -391,14 +350,7 @@ int main()
 	}
 
 	cout << "Опорный план" << endl; print_plan();
-	cout << "Целевая функция SV = " << setprecision(PRECISION) << SV << endl << endl;
-
-#ifdef CHECK_ALG
-	if (SV < 41.65 || SV > 41.67)
-	{
-		assert(!"CHECK_ALG failed!");
-	}
-#endif
+	cout << "Целевая функция SV = " << SV << endl << endl;
 
 	cout << "Число базисных клеток = " << kol_X;
 
@@ -419,6 +371,26 @@ int main()
 
 	//УЛУЧШЕНИЕ ОПОРНОГО ПЛАНА МЕТОДОМ ПОТЕНЦИАЛОВ---------------------------------
 
+	array_i p_U;
+	array_j p_V;
+
+	fill(begin(p_U), end(p_U), 0.0);
+	fill(begin(p_V), end(p_V), 0.0);
+
+	for (int i = 0; i < K; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			if (!isnan(plan[i][j]))
+			{
+				p_V[j] = C[i][j] - p_U[i];
+				p_U[i] = C[i][j] - p_V[j];
+			}
+		}
+	}
+
+	cout << "p_U[K]: " << p_U << endl;
+	cout << "p_V[N]: " << p_V << endl;
 
 	cin.get();
 	return 0;
